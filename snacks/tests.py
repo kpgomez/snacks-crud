c4864f8bbb0970b8e8efb4c36f08cf4483e18437
Kiengchay Gomez <kphoutthavong@gmail.com>
2023-12-16 23:24:57 -0600

    added some tests

--- snacks/tests.py
+++ snacks/tests.py
@@ -58,3 +58,38 @@ class SnacksReadTests(TestCase):
         url = reverse("snack_list")
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
+
+    def test_list_page_template(self):
+        url = reverse("snack_list")
+        response = self.client.get(url)
+        self.assertTemplateUsed(response, "snack_list.html")
+        self.assertTemplateUsed(response, "base.html")
+
+    def test_list_page_context(self):
+        url = reverse("snack_list")
+        response = self.client.get(url)
+        snacks = response.context["object_list"]
+        self.assertEqual(len(snacks), 1)
+        self.assertEqual(snacks[0].title, "pickle")
+        self.assertEqual(snacks[0].description, "vinegary")
+        self.assertEqual(snacks[0].purchaser, "tester")
+
+    def test_detail_page_status_code(self):
+        url = reverse("snack_detail", args=(1,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_detail_page_template(self):
+        url = reverse("snack_detail", args=(1,))
+        response = self.client.get(url)
+        self.assertTemplateUsed(response, "snack_detail.html")
+        self.assertTemplateUsed(response, "base.html")
+
+    def test_detail_page_context(self):
+        url = reverse("snack_detail", args=(1,))
+        response = self.client.get(url)
+        snack = response.context["snack"]
+        self.assertEqual(snack.title, "pickle")
+        # self.assertEqual(snack.description, "")
+        self.assertEqual(snack.purchaser.username, "tester")
+
